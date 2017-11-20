143fdae08fd8836e69a6152fbf03345187a7afcd
qiumin <50148363@qq.com>
2017-11-20 10:15:02 +0800

    remove generated files

--- www/app/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-const app_module_1 = require("./app.module");
-const core_1 = require("@angular/core");
-core_1.enableProdMode();
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
